1d1247ce55fa2dc12656930802e9769fd7bd19cf
Misha <sdvig96@gmail.com>
2017-12-23 17:36:30 +0200

    Create cxx_multiprocessing.cxx

---
+++ question/cxx_multiprocessing.cxx
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unistd.h>
+
+void pricess_func(){sleep(5); std::cout << "done" << std::endl;}
+
+int main (int argc, char * argv[]) {
+	
+	std::string text;
+	while (text != "exit") {
+		std::cout << "-> command ~$ "; std::cin >> text;
+		if (text == "list") {
+			if (text == "start") {
+				/* ??? */
+			}
+			else if (text == "stop") {
+				/* ??? */
+			}
+		}
+	}
+	return 0;
+}
